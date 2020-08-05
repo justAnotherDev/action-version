9e5da566d6d1b1271fe5d48b54c8cd6b40f682ac
Casey Evanoff <casey@scribd.com>
2020-08-04 18:20:13 -0600

    set updated version as a step output

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
-FROM opspresso/builder:alpine
-
-LABEL "com.github.actions.name"="Version Bump"
-LABEL "com.github.actions.description"="Increase the patch version."
-LABEL "com.github.actions.icon"="tag"
-LABEL "com.github.actions.color"="blue"
-
-LABEL version=v0.2.5
-LABEL repository="https://github.com/opspresso/action-version"
-LABEL maintainer="Jungyoul Yu <me@nalbam.com>"
-LABEL homepage="https://opspresso.com/"
+FROM alpine:3.10
 
 ADD entrypoint.sh /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,64 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-_error() {
-  echo -e "$1"
+if [ ! -f ./VERSION ]; then
+  printf "v0.0.x" > ./VERSION
+fi
 
-  if [ "${LOOSE_ERROR}" == "true" ]; then
-    exit 0
-  else
-    exit 1
-  fi
-}
+# release version
+MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
+MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
+PATCH=$(cat ./VERSION | xargs | cut -d'.' -f3)
 
-_version() {
-  if [ ! -f ./VERSION ]; then
-    printf "v0.0.x" > ./VERSION
-  fi
+PATCH=`expr $PATCH + 1`
 
-  echo "GITHUB_REF: ${GITHUB_REF}"
+VERSION="${MAJOR}.${MINOR}.${PATCH}"
+printf "${VERSION}" > ./VERSION
 
-  # release version
-  MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
-  MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
-  PATCH=$(cat ./VERSION | xargs | cut -d'.' -f3)
-
-  if [ "${PATCH}" != "x" ]; then
-    VERSION="${MAJOR}.${MINOR}.${PATCH}"
-    printf "${VERSION}" > ./VERSION
-  else
-    # latest versions
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
-    VERSION=$(curl -s ${URL} | jq -r '.[] | .tag_name' | grep "${MAJOR}.${MINOR}." | cut -d'-' -f1 | sort -Vr | head -1)
-
-    if [ -z ${VERSION} ]; then
-      VERSION="${MAJOR}.${MINOR}.0"
-    fi
-
-    echo "VERSION: ${VERSION}"
-
-    # new version
-    if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
-      VERSION=$(echo ${VERSION} | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-    else
-      if [ "${GITHUB_REF}" != "" ]; then
-        # refs/pull/1/merge
-        PR_CMD=$(echo "${GITHUB_REF}" | cut -d'/' -f2)
-        PR_NUM=$(echo "${GITHUB_REF}" | cut -d'/' -f3)
-      fi
-
-      if [ "${PR_CMD}" == "pull" ] && [ "${PR_NUM}" != "" ]; then
-        VERSION="${VERSION}-${PR_NUM}"
-      else
-        VERSION=""
-      fi
-    fi
-
-    if [ "${VERSION}" != "" ]; then
-      printf "${VERSION}" > ./VERSION
-    fi
-  fi
-
-  echo "VERSION: ${VERSION}"
-}
-
-_version
+echo "VERSION: $VERSION"
+echo "::set-output name=version::$VERSION"
